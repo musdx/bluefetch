d52113fe87f141e95d22c941661f6958f6d0b37d
musdx <yeriase@proton.me>
2025-04-06 17:56:24 -0400

    De added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use modules::de;
 use modules::user;
-
 mod modules {
     pub mod cpu;
+    pub mod de;
     pub mod mem;
     pub mod sysgen;
     pub mod user;
@@ -13,8 +14,12 @@ fn main() {
 
     println!("Os: {}", system_general_info.distro);
     println!("Host: {}@{}", user::user_name(), system_general_info.host);
+    println!("DE/WM: {}", de::get_de());
     println!("Kernel: {}", system_general_info.kernel);
-    println!("Memory: {:.2}/{:.2}", memory.mem_used, memory.mem_full);
+    println!(
+        "Memory: {:.2} GiB / {:.2} GiB",
+        memory.mem_used, memory.mem_full
+    );
     println!("Device: {}", system_general_info.mobo);
-    println!("CPU: {}", modules::cpu::cpuinfo())
+    println!("CPU: {}", modules::cpu::cpuinfo());
 }

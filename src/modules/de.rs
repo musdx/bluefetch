60a9ec52e0e83955ab18a762323a5f4b9a897282
musdx <yeriase@proton.me>
2025-04-06 17:59:07 -0400

    De added

--- src/modules/de.rs
+++ src/modules/de.rs
@@ -1,10 +1,8 @@
 use std::env;
 
 pub fn get_de() -> String {
-    env::var("XDG_SESSION_DESKTOP")
+    env::var("XDG_CURRENT_DESKTOP")
         .ok()
-        .or_else(|| env::var("DESKTOP_SESSION").ok())
-        .or_else(|| env::var("XDG_CURRENT_DESKTOP").ok())
         .or_else(|| env::var("CURRENT_DESKTOP").ok())
         .or_else(|| env::var("SESSION_DESKTOP").ok())
         .unwrap_or("N/A".to_string())

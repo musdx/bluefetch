33f2969d02b4be5a2de553685a4f8108ebca32a4
musdx <yeriase@proton.me>
2025-04-06 10:28:29 -0400

    Memory

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ mod modules {
 
 fn main() {
     let system_general_info = modules::sysgen::sys_check();
-
+    let memory = modules::mem::memory();
     println!("Os: {}", system_general_info.distro);
     println!("Host: {}@{}", user::user_name(), system_general_info.host);
     println!("Kernel: {}", system_general_info.kernel);
+    println!("Memory: {:.2}/{:.2}", memory.mem_used, memory.mem_full);
     println!("Device: {}", system_general_info.mobo);
     println!("CPU: {}", modules::cpu::cpuinfo())
 }
--- src/modules/mem.rs
+++ src/modules/mem.rs
@@ -1,15 +1,50 @@
-use std::fs;
+use std::{f64, fs};
 
-pub fn memory() {
-    let mut mem_full: u64 = 0;
-    let mut mem_free: u64 = 0;
+pub struct Sysmem {
+    pub mem_full: f64,
+    pub mem_used: f64,
+}
+
+pub fn memory() -> Sysmem {
+    let mut mem_full: f64 = 0.0;
+    let mut mem_free: f64 = 0.0;
 
     match fs::read_to_string("/proc/meminfo") {
         Ok(ouput) => {
-            for line in ouput.split(":") {
-                if line.trim().starts_with("MemTotal") {}
+            for line in ouput.split("\n") {
+                if line.trim().starts_with("MemTotal:") {
+                    mem_full = line
+                        .split(":")
+                        .nth(1)
+                        .unwrap()
+                        .trim()
+                        .split_whitespace()
+                        .next()
+                        .unwrap()
+                        .parse::<f64>()
+                        .unwrap()
+                        / (1024.0 * 1024.0);
+                } else if line.trim().starts_with("MemAvailable:") {
+                    mem_free = line
+                        .split(":")
+                        .nth(1)
+                        .unwrap()
+                        .trim()
+                        .split_whitespace()
+                        .next()
+                        .unwrap()
+                        .parse::<f64>()
+                        .unwrap()
+                        / (1024.0 * 1024.0);
+                    break;
+                }
             }
         }
-        Err(e) => {}
+        Err(e) => {
+            println!("{e}")
+        }
     }
+    let mem_used: f64 = mem_full - mem_free;
+
+    Sysmem { mem_full, mem_used }
 }
